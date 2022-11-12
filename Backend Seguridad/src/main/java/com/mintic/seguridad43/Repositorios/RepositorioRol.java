78871631a81ee678dbfc42664930d320fb50b163
ngomez07 <ngomez07@hotmail.com>
2022-11-12 00:02:30 -0500

    ddd

--- Backend Seguridad/src/main/java/com/mintic/seguridad43/Repositorios/RepositorioRol.java
+++
@@ -1,7 +0,0 @@
-package com.mintic.seguridad43.Repositorios;
-
-import com.mintic.seguridad43.Modelos.Rol;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RepositorioRol extends MongoRepository<Rol, String> {
-}

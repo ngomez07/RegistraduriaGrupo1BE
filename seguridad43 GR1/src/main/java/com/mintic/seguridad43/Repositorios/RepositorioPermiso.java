4d7b4c8cd66e8b03369333d4be3a0711baa18bc1
MiguelVel1995 <107654859+MiguelVel1995@users.noreply.github.com>
2022-11-11 23:53:33 -0500

    nuevos
    nuevos

--- seguridad43 GR1/src/main/java/com/mintic/seguridad43/Repositorios/RepositorioPermiso.java
+++
@@ -1,8 +0,0 @@
-package com.mintic.seguridad43.Repositorios;
-
-
-import com.mintic.seguridad43.Modelos.Permiso;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RepositorioPermiso extends MongoRepository<Permiso, String> {
-}
--- seguridad43 GR1/src/main/java/com/mintic/seguridad43/Repositorios/RepositorioPermisosRoles.java
+++
@@ -1,8 +0,0 @@
-package com.mintic.seguridad43.Repositorios;
-
-import com.mintic.seguridad43.Modelos.PermisosRoles;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RepositorioPermisosRoles extends MongoRepository<PermisosRoles, String> {
-
-}
